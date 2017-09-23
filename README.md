dec598724778862608d7db24c13f0c32eb4bea2f
Miles McCain <milesmcc@users.noreply.github.com>
2017-09-23 17:45:48 -0400

    initial push

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# senator-conservatism
+# Senator Conservatism
 Senators across time, ranked by conservatism.
+
+Created for Dr. Sarah Kreps.
+
+## Data Sources
+The raw DW-NOMINATE data was sourced from the official VoteView source, at ftp://k7moa.com/junkord/SL01113D21_BSSE.XLSX. It was then edited in Excel to add labels, which are visible in the file `data/data.xlsx`.
+
+## Methodology
+The Python program `rank.py` loads the DW-NOMINATE data and, for each congress, ranks its senators by their conservatism. Conservatism is defined by the the senator's 1st dimension DW-NOMINATE score, where a score of 1 is considered most conservative, and -1 is considered least conservative.
+
+The results are outputted the `congresses/` directory.
+
+## How to Run
+Simply run `python rank.py` with the current working directory set to this file's parent directory.
---
+++ rank.py
@@ -0,0 +1,42 @@
+class Legislator:
+    def __init__(self, congress, icpsr, state_code, district, state, party, name, dim1, dim2, dim1bootstrappedstandarderror, dim2bootstrappedstandarderror, correlation, loglikelihood, votes, errors, geomeanprobability):
+        self.congress = congress
+        self.icpsr = icpsr
+        self.state_code = state_code
+        self.district = district
+        self.state = state
+        self.party = party
+        self.name = name
+        self.dim1 = dim1
+        self.dim2 = dim2
+        self.dim1bootstrappedstandarderror = dim1bootstrappedstandarderror
+        self.dim2bootstrappedstandarderror = dim2bootstrappedstandarderror
+        self.correlation = correlation
+        self.loglikelihood = loglikelihood
+        self.votes = votes
+        self.errors = errors
+        self.geomeanprobability = geomeanprobability
+
+    def to_csv(self):
+        return(",".join([str(x) for x in [self.congress, self.icpsr, self.state_code, self.district, self.state, self.party, self.name, self.dim1, self.dim2, self.dim1bootstrappedstandarderror, self.dim2bootstrappedstandarderror, self.correlation, self.loglikelihood, self.votes, self.errors, self.geomeanprobability]]) + "\n")
+
+# load source data
+from openpyxl import load_workbook
+data = load_workbook('data/data.xlsx').active
+
+congresses = {}
+
+for row in data.rows:
+    legislator = Legislator(*[value.value for value in row])
+    # print dir(legislator.state.value)
+    if str(legislator.congress) not in congresses:
+        congresses[str(legislator.congress)] = []
+    congresses[str(legislator.congress)].append(legislator)
+
+for congress_name in congresses:
+    congress = congresses[congress_name]
+    by_conservatism = sorted(congress, key=lambda k: k.dim1, reverse=True)
+    with open("congresses/" + congress_name + ".csv", "wb") as outfile:
+        outfile.write("congress,icpsr,state_code,district,state,party,name,dim1,dim2,dim1bootstrappedstandarderror,dim2bootstrappedstandarderror,correlation,loglikelihood,votes,errors,geomeanprobability\n")
+        for legislator in by_conservatism:
+            outfile.write(legislator.to_csv())
